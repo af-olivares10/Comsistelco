423567858ca0dda0e7ec0cae62934f885f81ece2
Andrés <af.olivares10@uniandes.edu.co>
2019-01-12 19:39:37 -0500

    Make navbar customizable

--- assets/custom/main.js
+++ assets/custom/main.js
@@ -4,17 +4,81 @@
 				var results = regex.exec(window.location.search);
 				return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 			};
-			$(document).ready(function () {
-				$.get("https://intellgentcms.herokuapp.com/api/pages",data=>{
-					console.log(data);
-					let lastPages = data.pages.splice(4);
-					data.pages.forEach(p=>{
-						$("#page-menu-column-1").append(`<a  class='mbr-black text-black dropdown-item display-4 p-0 m-0' href='interna?name=${p.url}' aria-expanded="false">${p.name}</a>`);
-					})
-					lastPages.forEach(p=>{
-						$("#page-menu-column-2").append(`<a class="mbr-black text-black dropdown-item display-4 p-0 m-0" href="interna?name=${p.url}" aria-expanded="false">${p.name}</a>`);
-					})
 
+
+			$(document).ready(function () {
+				$.get("https://intellgentcms.herokuapp.com/api/pageCategories",data=>{
+					data.pageCategories.filter(pc=> pc.location ==="Barra").forEach((pc)=>{
+						$("#last-predefined-on-bar").after(`             
+						 <li class="nav-item dropdown work-please" >
+							<a class="nav-link link mbr-black text-black dropdown-toggle display-4" href="index.html" aria-expanded="true" data-toggle="dropdown-submenu">
+							${pc.name}</a>
+							<div class="dropdown-menu menu-doble">
+								<div class="container" style="padding: 0; margin: 0;">
+									<div class="row" style="padding: 0; margin: 0;">
+										<div class="col-md-6 col-12" id ="page-menu-column-1-${pc.name.replace(/\s/g, '')}" style="padding: 0; margin: 0;">
+										</div>
+										<div class="col-md-6 col-12" id ="page-menu-column-2-${pc.name.replace(/\s/g, '')}">
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>`);
+
+					})
+					//Categories inside a category, the father is placed on the bar
+					let underBarCategories = data.pageCategories.filter(pc=> pc.location !=="Barra");
+					underBarCategories.forEach((pc)=>{
+						$(`#page-menu-column-2-${pc.location.replace(/\s/g, '')}`).append(`           
+
+                		<div class="dropdown">
+                	  		<a class="mbr-black text-black dropdown-item dropdown-toggle display-4" href="index.html" data-toggle="dropdown-submenu" aria-expanded="true">${pc.name}</a>
+                  			<div class="dropdown-menu dropdown-submenu" id = "under-bar-category-${pc.name.replace(/\s/g, '')}">
+                 			 </div>
+                		</div>
+
+                `);
+				})
+
+					$.get("https://intellgentcms.herokuapp.com/api/pages",data=>{
+
+						data.pages.filter(p=>p.pageCategory==="Barra").forEach(p=>{
+							$("#last-predefined-on-bar").after(`
+								<li class="nav-item work-please">
+                					<a class="nav-link link mbr-black text-black display-4" href='interna?name=${p.url}'>${p.name}</a>
+              					</li>
+								`);
+						})
+							//pages inside categories inside a category, the father category is placed on the bar
+							let underUnderBarPages = data.pages.filter(p=> underBarCategories.find(  pc=> pc.name===p.pageCategory) )
+							
+							//pages inside a category placed on the bar 
+							let underBarPages = data.pages.filter(p=> !underBarCategories.find(pc=>pc.name===p.pageCategory) )
+
+							//pages inside a category placed on the bar x2
+							let lastUnderBarPages = underBarPages.splice(5);
+							underBarPages.forEach(p=>{
+							$(`#page-menu-column-1-${p.pageCategory.replace(/\s/g, '')}`).append(`<a class="mbr-black text-black dropdown-item display-4 " href="interna?name=${p.url}" >${p.name}</a>`);
+						})
+						lastUnderBarPages.forEach(p=>{
+							$(`#page-menu-column-2-${p.pageCategory.replace(/\s/g, '')}`).append(`<a class="mbr-black text-black dropdown-item display-4 p-0 m-0" href="interna?name=${p.url}" aria-expanded="false">${p.name}</a>`);
+						})
+						underUnderBarPages.forEach(p=>{
+							$(`#under-bar-category-${p.pageCategory.replace(/\s/g, '')}`).append(`<a class="mbr-black text-black dropdown-item display-4 " href="interna?name=${p.url}" >${p.name}</a>`);
+						})
+
+						$(".dropdown").mouseover((event)=>{$(event.currentTarget).addClass("open") })
+						$(".dropdown").mouseout((event)=>{ $(event.currentTarget).removeClass("open") })
+						$(".dropdown").mouseout((event)=>{ $(event.currentTarget).removeClass("open") })
+
+										$(".nav-item").mouseenter((e)=>{
+					$("js-float-line").append('<li class="main-menu-animated-line bottom"></li>')
+					const left = $(event.currentTarget).position().left				
+					const width = $(event.currentTarget).width()				
+					$(".main-menu-animated-line ").attr("style",`width: ${width}px; left:${left}px;`)
+			})
+
+					})
 				})
 				$.get(`https://intellgentcms.herokuapp.com/api/page?url=${getUrlParameter("name")}`,data=>{
 					//console.log(data);
@@ -37,5 +101,5 @@
 					})
 
 				})
-			});
 
+		});
